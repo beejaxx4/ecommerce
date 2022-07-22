7657625e92a0dc7e1ac8269ab3f0bf381016a8ee
beejaxx4 <mobolajisiyanbola@yahoo.com>
2022-07-22 12:06:29 +0100

    domain

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-oo*dna)sg7$*i)vo5rsksmu_6yh!vy(j-t8!(5gv2o_!jx-2n^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'ecommerce-23.herokuapp.com']
 
 
 # Application definition
